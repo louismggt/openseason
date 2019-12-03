d10c9c497c39e70fff1b4f9973ad37f25377b724
louis marie guiganton <louismarie.guiganton@gmail.com>
2019-12-03 10:40:33 +0100

    corrected applicationjs

---
+++ app/javascript/plugins/init_animtext.js
@@ -0,0 +1,24 @@
+
+const textAnimator = () => {
+  const textWrapper = document.querySelector('.ml6 .letters');
+  if (textWrapper) {
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: true})
+      .add({
+        targets: '.ml6 .letter',
+        translateY: ["1.1em", 0],
+        translateZ: 0,
+        duration: 750,
+        delay: (el, i) => 50 * i
+      }).add({
+        targets: '.ml6',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+      });
+    }
+}
+
+export { textAnimator };
