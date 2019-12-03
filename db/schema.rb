96a281d5e35cc26e58d092ff109c8d1ab46aa2bf
louis marie guiganton <louismarie.guiganton@gmail.com>
2019-12-03 14:25:59 +0100

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_113229) do
+ActiveRecord::Schema.define(version: 2019_12_03_101518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_113229) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "message"
     t.index ["job_id"], name: "index_missions_on_job_id"
     t.index ["user_id"], name: "index_missions_on_user_id"
   end
