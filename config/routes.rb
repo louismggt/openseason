f9b9ec40f10e60779f0d12bf334c150460e7f057
Pierre Danckaert pierre.danckaert@njoy.fr <pierre.danckaert@njoy.fr>
2019-12-03 14:50:16 +0100

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "toto", to: 'pages#toto'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :profils, only: [:show]
   resources :preferences
