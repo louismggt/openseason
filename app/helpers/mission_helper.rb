97b66fc6c809f8f6e5cfdd4fb4dc53ff1d2fb7c1
louis marie guiganton <louismarie.guiganton@gmail.com>
2019-12-05 14:00:50 +0100

    application status implanted on mini cards

---
+++ app/helpers/mission_helper.rb
@@ -0,0 +1,3 @@
+module MissionHelper
+
+end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,9 @@
 class Mission < ApplicationRecord
   belongs_to :job
   belongs_to :user
+
+  def application
+    return "En attente" if self.status == "pending"
+    return "Candidature validée!"if self.status == "validated"
+  end
 end
